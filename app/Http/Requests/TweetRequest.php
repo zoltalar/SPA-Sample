6b44c8e1bcab0bab04a6c6d7c13e6beafd1ed048
Wojciech Pirog <wojciech.pirog@polcode.net>
2019-01-29 10:09:46 +0100

    overriding tweet request to automatically include 'user_id' value

--- app/Http/Requests/TweetRequest.php
+++ app/Http/Requests/TweetRequest.php
@@ -6,6 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class TweetRequest extends FormRequest
 {
+    // Override
+    protected function validationData()
+    {
+        return array_merge($this->all(), [
+            'user_id' => $this->user()->id
+        ]);
+    }
+
     public function authorize()
     {
         return true;
